05aca17f2a6fcc283abddfee6b1a1e7b85866a21
Remon Samir <remonsamir532@gmail.com>
2026-01-03 10:40:09 +0200

    added the second task 104

--- tests/automationExerciseBignner.spec.ts
+++ tests/automationExerciseBignner.spec.ts
@@ -1,4 +1,4 @@
-import {test,expect} from '@playwright/test';
+import {test,expect, Locator} from '@playwright/test';
 
 test('Task 103', async ({page})=>{
     await page.goto(" https://www.tutorialspoint.com/selenium/practice/text-box.php");
@@ -8,4 +8,27 @@ test('Task 103', async ({page})=>{
     await impressiveRadioButton.check();
     const assertMessage = page.locator("//div[@id='check1']");
    await expect(assertMessage).toHaveText("You have checked Impressive");
+});
+
+test('Task 104', async ({page})=>{
+    await page.goto(" https://www.tutorialspoint.com/selenium/practice/text-box.php");
+    const webTableButton = page.locator("//a[@href='webtables.php']");
+    await webTableButton.click();
+    await page.getByRole("button",{name :'Add'}).click();
+    await page.getByPlaceholder("First Name").fill('test firstname');
+    await page.getByPlaceholder("Last Name").fill('test lastname');
+    await page.getByPlaceholder("Enter Email").fill('testemail@gmail.com');
+    await page.getByPlaceholder("Enter Age").fill('test age');
+    await page.getByPlaceholder("Enter Salary").fill('test expected or actual salary');
+    await page.getByPlaceholder("Enter Department").fill('test deparment');
+    await page.screenshot({path : 'screenShots/formScreen.png'})
+    await page.getByRole("button", {name : 'Login'}).click();
+    const deleteButtons = page.locator("//*[@data-icon='trash']");
+    await deleteButtons.first().waitFor({ state: 'visible' });
+    while (await deleteButtons.count() > 0) {
+    await deleteButtons.first().click();
+    }
+    await expect(deleteButtons).toHaveCount(0);
+    await page.pause();
+
 });
